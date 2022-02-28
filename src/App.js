6f3d4240acee7d1c6b73fd40599dddc40d15497b
Bleacheddata <Masoodahmedkk@gmail.com>
2022-02-28 12:29:59 +0000

    Created card styles and animations

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ export default class App extends React.Component {
     super(props);
     this.state = { pack: [] };
     this.openPack = this.openPack.bind(this);
+    this.onHover = this.onHover.bind(this);
+    this.onLeaveHover = this.onLeaveHover.bind(this);
   }
   render() {
     return (
@@ -14,15 +16,53 @@ export default class App extends React.Component {
         <button onClick={this.openPack}>
           <h1>Open Card Pack</h1>
         </button>
-        {this.state.pack.length}
+        <div className="cards-container">
+          {this.state.pack.map((i, index) => (
+            <div
+              key={index}
+              id={index}
+              onMouseOver={() => this.onHover(index, i.border)}
+              onMouseLeave={() => this.onLeaveHover(index)}
+              className="frame"
+              style={{
+                background: `url(${i.image}) no-repeat center center / cover`,
+                border: `0.5rem outset ${i.border}`
+              }}
+            >
+              <div className="cardtop white">
+                {i.rarity} <br />
+                {i.name}
+              </div>
+              <div className="cardmid white">{i.type}</div>
 
-        {this.state.pack.map((i) => (
-          <div>{i.name}</div>
-        ))}
+              <div className="power white">{i.power} </div>
 
-        <div className="cardPack"></div>
+              <div className="texture"> </div>
+            </div>
+          ))}
+        </div>
       </div>
     );
+  }
+
+  onHover(id, color) {
+    let card = document.getElementById(id).style;
+    card.transitionDuration = "0.5s";
+    card.width = "16vmax";
+    card.height = "26.66vmax";
+    card.boxShadow = `0 0 20px 10px ${color}`;
+
+    if (color === "gold") {
+      card.boxShadow = `0 0 50px 30px ${color}`;
+    }
+  }
+
+  onLeaveHover(id) {
+    let card = document.getElementById(id).style;
+    card.transitionDuration = "0.2s";
+    card.width = "15vmax";
+    card.height = "25vmax";
+    card.boxShadow = ``;
   }
 
   openPack() {
--- src/cards.js
+++ src/cards.js
@@ -1,43 +1,87 @@
-function randomInteger(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+function randomInteger(max) {
+  return Math.floor(Math.random() * max);
 }
 
 let cardNames = [
-  "Raiden",
-  "Eden",
-  "Ghost",
-  "Nightrunner",
-  "Archangel",
-  "Scorch",
-  "Tempest",
-  "Phantom",
-  "Knight",
-  "Viper"
+  {
+    name: "Raiden",
+    image:
+      "https://i.pinimg.com/originals/71/ff/e2/71ffe2e22795d6ca9b80da4e9fd154c8.jpg"
+  },
+  {
+    name: "Phoenix",
+    image:
+      "https://i.pinimg.com/originals/4e/43/09/4e4309728598bfc1b06d6681e38502ff.jpg"
+  },
+  {
+    name: "Ghost",
+    image:
+      "https://i.pinimg.com/originals/6b/f4/da/6bf4da99007d304f76d46ff8e7f7ad46.jpg"
+  },
+  {
+    name: "Nightrunner",
+    image:
+      "https://i.pinimg.com/736x/18/ed/74/18ed74b95a0c3856087ff62b9de76175--cyberpunk-character-shooter-games.jpg"
+  },
+  {
+    name: "Archangel",
+    image:
+      "https://i.pinimg.com/originals/7f/17/4d/7f174d7f31ac58a40ef4447bf78af93b.jpg"
+  },
+  {
+    name: "Scorch",
+    image:
+      "https://i.pinimg.com/originals/4f/9c/ef/4f9cef22478edd093eb6a74182758c6c.jpg"
+  },
+  {
+    name: "Tempest",
+    image:
+      "https://i.pinimg.com/originals/c6/a8/9d/c6a89d990f0055ec4035db19c0df0668.jpg"
+  },
+  {
+    name: "Phantom",
+    image:
+      "https://i.pinimg.com/originals/52/e4/88/52e48892c471b8575b71b2ec81d21538.jpg"
+  },
+  {
+    name: "Viper",
+    image:
+      "https://i.pinimg.com/736x/cd/3e/2b/cd3e2bffa7f01d4b9ab8fc4a016bef93.jpg"
+  },
+  {
+    name: "Nomad",
+    image:
+      " https://i.pinimg.com/originals/50/fa/c7/50fac7e383458e9360faa499f27819d1.jpg"
+  }
 ];
 let cardType = ["Depletor", "Generator"];
 
 export function getCard() {
-  let card = Object();
-  card.name = cardNames[randomInteger(0, cardNames.length)];
-  card.power = randomInteger(0, 12);
+  let card = cardNames[randomInteger(cardNames.length)];
+  card.power = randomInteger(12);
 
   if (card.power >= 0 && card.power <= 3) {
     card.rarity = "Common";
+    card.border = "white";
   }
   if (card.power >= 4 && card.power <= 6) {
     card.rarity = "Uncommon";
+    card.border = "lightgreen";
   }
   if (card.power >= 7 && card.power <= 9) {
     card.rarity = "Rare";
+    card.border = "lightskyblue";
   }
   if (card.power >= 10 && card.power <= 12) {
     card.rarity = "Ultra-Rare";
+    card.border = "gold";
   }
 
-  card.type = cardType[randomInteger(0, 1)];
+  card.type = cardType[randomInteger(cardType.length)];
 
   if (card.type === "Depletor") {
     card.power = card.power * -1;
   }
+  console.log(card);
   return card;
 }
